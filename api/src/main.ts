23c4640686b09d77a7fdfe66b9fc63cb1741afd5
xueyang <313867808@qq.com>
2026-04-13 02:27:33 +0800

    fix(api): adjust ai service configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,12 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-import axios from 'axios';
-axios.post('*REMOVED-URL*').then((res) => {
-  console.log(res)
-}).catch(err => {
-  console.error(err)
-})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
